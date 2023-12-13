b1d03e629ddbf48702e7195110bf7f6b0bb2eda6
JarGad23 <j.gad@m8b.pl>
2023-12-13 16:03:47 +0100

    css global changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,10 @@
 import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
-  return <Button>Clicke me!</Button>;
+  return (
+    <div className="flex flex-col gap-y-4">
+      <UserButton afterSignOutUrl="/" />
+    </div>
+  );
 }
