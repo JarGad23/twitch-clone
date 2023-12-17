255c7b0c27bbea60ae4b4d80eff114d2c02f0e75
JarGad23 <j.gad@m8b.pl>
2023-12-17 21:39:41 +0100

    Modified navbar to handle dashboard ui

--- app/(browse)/_components/navbar/index.tsx
+++ app/(browse)/_components/navbar/index.tsx
@@ -2,12 +2,16 @@ import { Actions } from "./actions";
 import { Logo } from "./logo";
 import { Search } from "./search";
 
-export const Navbar = () => {
+interface NavbarProps {
+  isInDashboardView: boolean;
+}
+
+export const Navbar = ({ isInDashboardView }: NavbarProps) => {
   return (
     <nav className="fixed top-0 w-full h-20 z-[49] bg-[#252731] px-2 lg:px-4 flex justify-between items-center shadow-sm">
-      <Logo />
-      <Search />
-      <Actions />
+      <Logo isInDashboardView={isInDashboardView} />
+      {!isInDashboardView ? <Search /> : null}
+      <Actions isInDashboardView={isInDashboardView} />
     </nav>
   );
 };
