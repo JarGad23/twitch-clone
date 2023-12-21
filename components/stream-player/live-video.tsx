d11aab061a9ac9451b4a6902e8022e8ec6b9d614
Jarosław Gad <127743272+JarGad23@users.noreply.github.com>
2023-12-21 20:59:48 +0100

    Feature 24: Finish video component (#26)
    * Pacakge json changes
    
    * Added volume control component
    
    * Live video finishing
    
    * Added slider component

--- components/stream-player/live-video.tsx
+++ components/stream-player/live-video.tsx
@@ -1,9 +1,11 @@
 "use client";
 
 import { Participant, Track } from "livekit-client";
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useTracks } from "@livekit/components-react";
 import { FullScreenControl } from "./full-screen-control";
+import { useEventListener } from "usehooks-ts";
+import { VolumeControl } from "./volume-control";
 
 interface LiveVideoProps {
   participant: Participant;
@@ -14,6 +16,46 @@ export const LiveVideo = ({ participant }: LiveVideoProps) => {
   const wrapperRef = useRef<HTMLDivElement>(null);
 
   const [isFullScreen, setIsFullScreen] = useState(false);
+  const [volume, setVolume] = useState(0);
+
+  const toggleFullScreen = () => {
+    if (isFullScreen) {
+      document.exitFullscreen();
+    } else if (wrapperRef.current) {
+      wrapperRef.current.requestFullscreen();
+    }
+  };
+
+  const onVolumeChange = (value: number) => {
+    setVolume(+value);
+
+    if (videoRef.current) {
+      videoRef.current.muted = value === 0;
+      videoRef.current.volume = +value * 0.01;
+    }
+  };
+
+  const toggleMute = () => {
+    const isMuted = volume === 0;
+
+    setVolume(isMuted ? 50 : 0);
+
+    if (videoRef.current) {
+      videoRef.current.muted = !isMuted;
+      videoRef.current.volume = isMuted ? 0.5 : 0;
+    }
+  };
+
+  useEffect(() => {
+    onVolumeChange(0);
+  }, []);
+
+  const handleFullScreenChange = () => {
+    const isCurrentFullScreen = document.fullscreenElement !== null;
+    setIsFullScreen(isCurrentFullScreen);
+  };
+
+  useEventListener("fullscreenchange", handleFullScreenChange, wrapperRef);
 
   useTracks([Track.Source.Camera, Track.Source.Microphone])
     .filter((track) => track.participant.identity === participant.identity)
@@ -28,7 +70,15 @@ export const LiveVideo = ({ participant }: LiveVideoProps) => {
       <video ref={videoRef} width="100%" />
       <div className="absolute top-0 h-full w-full opacity-0 hover:opacity-100 transition-all">
         <div className="absolute bottom-0 flex h-14 w-full items-center justify-between bg-gradient-to-r from bg-neutral-900 px-4">
-          <FullScreenControl isFullScreen={false} onToggle={() => {}} />
+          <VolumeControl
+            onChange={onVolumeChange}
+            value={volume}
+            onToggle={toggleMute}
+          />
+          <FullScreenControl
+            isFullScreen={isFullScreen}
+            onToggle={toggleFullScreen}
+          />
         </div>
       </div>
     </div>
