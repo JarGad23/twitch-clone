9bbd8aad55bcf711cf7364fd6ed0771ce2d6073b
Jarosław Gad <127743272+JarGad23@users.noreply.github.com>
2023-12-23 16:08:31 +0100

    Feature 27: Community component (#32)
    * Pacakge json changes
    
    * Added chat communty components
    
    * Added scroll area
    
    * Added loading states to stramplayer components

--- components/stream-player/chat-header.tsx
+++ components/stream-player/chat-header.tsx
@@ -18,9 +18,9 @@ export const ChatHeader = () => {
   );
 };
 
-export const ChatSkeleton = () => {
+export const ChatHeaderSkeleton = () => {
   return (
-    <div className="relative p-3 border-b hidden md:visible">
+    <div className="relative p-3 border-b hidden md:block">
       <Skeleton className="absolute h-6 w-6 left-3 top-3" />
       <Skeleton className="w-28 mx-auto h-6" />
     </div>
