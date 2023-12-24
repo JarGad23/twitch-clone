2c940d2b2af9fb84a9db49d417070b5196e0b262
JarGad23 <j.gad@m8b.pl>
2023-12-24 15:29:27 +0100

    Added Info card and info card modal form

---
+++ components/stream-player/info-modal.tsx
@@ -0,0 +1,83 @@
+"use client";
+
+import { useState, useTransition, useRef, ElementRef } from "react";
+import {
+  Dialog,
+  DialogTrigger,
+  DialogContent,
+  DialogClose,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog";
+import { Button } from "@/components/ui/button";
+import { Label } from "@/components/ui/label";
+import { Input } from "@/components/ui/input";
+import { updateStream } from "@/actions/stream";
+import { toast } from "sonner";
+
+interface InfoModalProps {
+  initialName: string;
+  initialThumbnailurl: string | null;
+}
+
+export const InfoModal = ({
+  initialName,
+  initialThumbnailurl,
+}: InfoModalProps) => {
+  const [name, setName] = useState(initialName);
+  const [isPending, startTransition] = useTransition();
+
+  const closeRef = useRef<ElementRef<"button">>(null);
+
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setName(e.target.value);
+  };
+
+  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    startTransition(() => {
+      updateStream({ name })
+        .then(() => {
+          toast.success("Stream updated");
+          closeRef.current?.click();
+        })
+        .catch(() => toast.error("Something went wrong"));
+    });
+  };
+
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button variant="link" size="sm" className="ml-auto">
+          Edit
+        </Button>
+      </DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Edit stream info</DialogTitle>
+        </DialogHeader>
+        <form className="space-y-14" onSubmit={onSubmit}>
+          <div className="space-y-2">
+            <Label>Name</Label>
+            <Input
+              placeholder="Stream name"
+              onChange={onChange}
+              value={name}
+              disabled={isPending}
+            />
+          </div>
+          <div className="flex justify-between">
+            <DialogClose asChild ref={closeRef}>
+              <Button type="button" variant="ghost">
+                Cancel
+              </Button>
+            </DialogClose>
+            <Button variant="primary" type="submit" disabled={isPending}>
+              Save
+            </Button>
+          </div>
+        </form>
+      </DialogContent>
+    </Dialog>
+  );
+};
