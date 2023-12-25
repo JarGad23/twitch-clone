a0fc962e41fed9aecc84c871b64c9558fc4b5d41
JarGad23 <j.gad@m8b.pl>
2023-12-25 11:32:13 +0100

    Developed user page and changed things in chat components

--- app/(browse)/[username]/page.tsx
+++ app/(browse)/[username]/page.tsx
@@ -1,8 +1,8 @@
 import { isFollowingUser } from "@/lib/follow-service";
 import { getUserByUsername } from "@/lib/user-service";
 import { notFound } from "next/navigation";
-import { Actions } from "./_components/actions";
 import { isBlockedByUser } from "@/lib/block-service";
+import { StreamPlayer } from "@/components/stream-player";
 
 interface UsernamePageProps {
   params: {
@@ -13,25 +13,19 @@ interface UsernamePageProps {
 const UsernamePage = async ({ params }: UsernamePageProps) => {
   const user = await getUserByUsername(params.username);
 
-  if (!user) {
+  if (!user || !user.stream) {
     notFound();
   }
 
   const isFollowig = await isFollowingUser(user.id);
-  const isBlockByCurrentUser = await isBlockedByUser(user.id);
+  const isBlocked = await isBlockedByUser(user.id);
 
-  // if (isBlockByCurrentUser) {
-  //   notFound();
-  // }
+  if (isBlocked) {
+    notFound();
+  }
 
   return (
-    <div className="flex flex-col gap-y-4">
-      <p>Username: {user.username}</p>
-      <p>UsedID: {user.id}</p>
-      <p>isFollowing: {`${isFollowig}`}</p>
-      <p>Is blocked by current user: {`${isBlockByCurrentUser}`} </p>
-      <Actions isFollowing={isFollowig} userId={user.id} />
-    </div>
+    <StreamPlayer user={user} stream={user.stream} isFollowing={isFollowig} />
   );
 };
 
