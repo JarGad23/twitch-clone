e5e25f53c95a29cfd65bc22a24c3890ba7a42d31
JarGad23 <j.gad@m8b.pl>
2023-12-25 11:31:10 +0100

    modified block service

--- actions/block.ts
+++ actions/block.ts
@@ -1,16 +1,37 @@
 "use server";
 
+import { getSelf } from "@/lib/auth-service";
 import { blockUser, unBlockUser } from "@/lib/block-service";
+import { RoomServiceClient } from "livekit-server-sdk";
 import { revalidatePath } from "next/cache";
 
+if (
+  !process.env.LIVEKIT_API_URL ||
+  !process.env.LIVEKIT_API_KEY ||
+  !process.env.LIVEKIT_API_SECRET
+) {
+  throw new Error("Please provide API keys");
+}
+
+const roomService = new RoomServiceClient(
+  process.env.LIVEKIT_API_URL,
+  process.env.LIVEKIT_API_KEY,
+  process.env.LIVEKIT_API_SECRET
+);
+
 export const onBlock = async (id: string) => {
-  const blockedUser = await blockUser(id);
+  const self = await getSelf();
 
-  revalidatePath("/");
+  let blockedUser;
+  try {
+    blockedUser = await blockUser(id);
+  } catch (error) {}
 
-  if (blockedUser) {
-    revalidatePath(`/${blockedUser.blocked.username}`);
-  }
+  try {
+    await roomService.removeParticipant(self.id, id);
+  } catch (error) {}
+
+  revalidatePath(`/u/${self.username}/community`);
 
   return blockedUser;
 };
